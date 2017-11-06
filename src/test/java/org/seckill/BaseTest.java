cbfec18f46333566e1a7094782b97a12885adea8
DESKTOP-PCR84PE\单耀 <sqshanyao@163.com>
2017-11-06 23:26:07 +0800

    xml配置Spring注入service，service测试类

---
+++ src/test/java/org/seckill/BaseTest.java
@@ -0,0 +1,7 @@
+package org.seckill;
+
+/**
+ * Created by 单耀 on 2017/11/6.
+ */
+public class BaseTest {
+}
---
+++ src/test/java/org/seckill/service/Impl/SecKillServiceImplTest.java
@@ -0,0 +1,31 @@
+package org.seckill.service.Impl;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by 单耀 on 2017/11/6.
+ */
+public class SecKillServiceImplTest {
+    @Test
+    public void getSeckillList() throws Exception {
+
+    }
+
+    @Test
+    public void getById() throws Exception {
+
+    }
+
+    @Test
+    public void exportSeckillUrl() throws Exception {
+
+    }
+
+    @Test
+    public void executeSeckill() throws Exception {
+
+    }
+
+}
